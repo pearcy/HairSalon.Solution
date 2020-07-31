b43c1670b8f2d2a4b016349f5338228646ba4580
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-31 13:55:11 -0700

    add delete to Clients Controller

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -56,6 +56,21 @@ namespace HairSalon.Controllers
      return RedirectToAction("Index");
    }
 
+        public ActionResult Delete(int id)
+       {
+         var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+         return View(thisClient);
+       }
+
+       [HttpPost, ActionName("Delete")]
+       public ActionResult DeleteConfirmed(int id)
+       {
+         var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+         _db.Clients.Remove(thisClient);
+         _db.SaveChanges();
+         return RedirectToAction("Index");
+       }
+
 
 
 
